4653e1af79ea5eed810b90e055ee67b282d4ed9b
yz1201 <15957121194@163.com>
2020-08-13 22:46:17 +0800

    ti -st 斐波那契，二进制中1的个数 n&(n-1)

---
+++ src/main/java/cn/dbdj1201/interview/Prob9.java
@@ -0,0 +1,28 @@
+package cn.dbdj1201.interview;
+
+/**
+ * @author yz1201
+ * @date 2020-08-13 22:20
+ **/
+public class Prob9 {
+
+    public static void main(String[] args) {
+        /*面试题 9：斐波那契数列*/
+        System.out.println(fib(3));
+        System.out.println(fib(4));
+        System.out.println(fib(5));
+        System.out.println(fib(6));
+        System.out.println(fib(7));
+    }
+
+    private static int fib(int num) {
+        if (num < 1) {
+            return 0;
+        }
+
+        if (num == 1 || num == 2) {
+            return 1;
+        }
+        return fib(num - 1) + fib(num - 2);
+    }
+}
