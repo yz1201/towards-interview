5e65ae6e9e840ae61462cea348811f088107c6e7
yz1201 <15957121194@163.com>
2020-08-17 19:32:17 +0800

    ti -st Problem13 ,14

--- src/main/java/cn/dbdj1201/interview/Prob13.java
+++ src/main/java/cn/dbdj1201/interview/Prob13.java
@@ -1,6 +1,8 @@
 package cn.dbdj1201.interview;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -16,20 +18,81 @@ public class Prob13 {
             给定单向链表的头指针和一个结点指针，定义一个函数在 O(1)时间删除
         该结点。
         */
+        LinkedList13 list = generateList();
+        System.out.println(list);
+        INode13 next = list.head.next;
+        System.out.println(delete(list.head, list.head));
+//        System.out.println(list);
+
+
     }
 
-    private static void test() {
+    private static INode13 delete(INode13 head, INode13 node13) {
+        INode13 temp = head;
+        if (temp == node13) {
+            head = temp.next;
+            return head;
+        }
+        while (temp.next != node13) {
+            temp = temp.next;
+        }
+        temp.next = node13.next;
+        return head;
+    }
 
+    private static LinkedList13 generateList() {
+        INode13 node1 = new INode13(1);
+        INode13 node2 = new INode13(2);
+        INode13 node3 = new INode13(3);
+        INode13 node4 = new INode13(4);
+        LinkedList13 list = new LinkedList13();
+        list.add(node1);
+        list.add(node2);
+        list.add(node3);
+        list.add(node4);
+        return list;
     }
 }
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+class LinkedList13 {
+
+    INode13 head;
+
+    void add(INode13 node13) {
+        if (this.head == null) {
+            head = node13;
+            return;
+        }
+
+        INode13 temp = this.head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node13;
+    }
+
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 class INode13 {
 
     int num;
 
     INode13 next;
 
+    public INode13(int num) {
+        this.num = num;
+    }
 
-
+//    @Override
+//    public String toString() {
+//        return "INode13{" +
+//                "num=" + num +
+//                '}';
+//    }
 }
---
+++ src/main/java/cn/dbdj1201/interview/Prob14.java
@@ -0,0 +1,72 @@
+package cn.dbdj1201.interview;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+/**
+ * @Author: dbdj1201
+ * @Date: 2020-08-17 16:43
+ */
+@Slf4j
+public class Prob14 {
+
+    public static void main(String[] args) {
+        /*
+             面试题 14 ：调整数组顺序使奇数位于偶数前面
+        题目：输入一个整数数组，实现一个函数来调整该函数数组中数字的顺序，使得
+        所有奇数位于数组的前半部分，所有的数组位于数组的后半部分。
+        */
+        int[] nums = {1, 2, 3, 4};
+//        test2(nums);
+        test(nums);
+    }
+
+    private static void test(int[] nums) {
+        int len = nums.length;
+        int[] sortedNums = new int[len];
+
+        int left = 0;
+        int right = len - 1;
+
+        int index =0;
+        while (index < len) {
+            if (nums[index] % 2 == 0) {
+                sortedNums[right] = nums[index];
+                log.info("add num right-{}", nums[index]);
+                right--;
+            } else {
+                sortedNums[left] = nums[index];
+                left++;
+                log.info("add num left -{}", nums[index]);
+            }
+//            right--;
+            index++;
+        }
+
+        System.out.println(Arrays.toString(sortedNums));
+
+    }
+
+    private static void test2(int[] nums) {
+        int len = nums.length;
+        int[] odds = new int[len];
+        int[] edds = new int[len];
+        int oddIndex = 0;
+        int eddIndex = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                odds[oddIndex++] = num;
+            } else {
+                edds[eddIndex++] = num;
+            }
+        }
+        int[] result = new int[len];
+        System.arraycopy(edds, 0, result, 0, eddIndex);
+        System.arraycopy(odds, 0, result, eddIndex, oddIndex);
+
+        System.out.println(Arrays.toString(result));
+
+    }
+
+}
