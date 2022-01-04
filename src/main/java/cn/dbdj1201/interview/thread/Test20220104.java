a9d362454b285ccd73d5a7e4031f4233837cc6d0
yz1201 <15957121194@163.com>
2022-01-04 23:30:45 +0800

    leetcode two; multi thread test;

--- src/main/java/cn/dbdj1201/interview/thread/Test20220104.java
+++ src/main/java/cn/dbdj1201/interview/thread/Test20220104.java
@@ -1,5 +1,9 @@
 package cn.dbdj1201.interview.thread;
 
+import lombok.SneakyThrows;
+
+import java.util.Queue;
+
 /**
  * @Author: yz1201
  * @Date: 2022/1/4 22:44
@@ -27,32 +31,43 @@ public class Test20220104 {
 
         thread1.start();
         thread2.start();
+//        MyThread runnable = new MyThread();
+//        new Thread(runnable).start();
+//        new Thread(runnable).start();
     }
-
 
 
 }
 
-class MyThread implements Runnable{
+class MyThread implements Runnable {
+    int count = 0;
 
+    @SneakyThrows
     @Override
     public void run() {
-
+        synchronized (this) {
+            System.out.println(this);
+            int len = 10;
+            for (int i = 0; i < len; i++) {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + "-" + count++);
+            }
+        }
     }
 }
 
-class TestClass{
+class TestClass {
 
     int count = 0;
 
     public void testMethod() throws InterruptedException {
-        Object lock = new Object();
-        synchronized (lock){
-            System.out.println(lock);
-            int len =10;
+//        Object lock = new Object();
+        synchronized (this) {
+            System.out.println(this);
+            int len = 10;
             for (int i = 0; i < len; i++) {
-                Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + "-"+count++);
+                Thread.sleep(200);
+                System.out.println(Thread.currentThread().getName() + "-" + count++);
             }
         }
 
