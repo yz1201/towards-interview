203c89e66bbdf7ce2e6e098c089d234785b47ca7
yz1201 <15957121194@163.com>
2022-01-10 09:39:08 +0800

    juc;

---
+++ src/main/java/cn/dbdj1201/interview/thread/CountdownLatchTest.java
@@ -0,0 +1,53 @@
+package cn.dbdj1201.interview.thread;
+
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: yz1201
+ * @Date: 2022/1/7 8:46
+ */
+public class CountdownLatchTest {
+
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(3);
+
+        Thread[] threads = new Thread[100];
+        System.out.println("game start");
+
+        int len = 100;
+
+//        for (int i = 0; i < len; i++) {
+//            threads[i] = new Thread(()->{
+//                System.out.println();
+//                latch.countDown();
+//            });
+//        }
+
+        new Thread(() -> {
+            try {
+                int cycleTimes = 3;
+                for (int i = 0; i < cycleTimes; i++) {
+                    TimeUnit.SECONDS.sleep(1);
+                    latch.countDown();
+                    System.out.println("count down");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+
+//        Arrays.stream(threads).forEach(Thread::start);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("game over");
+    }
+
+
+}
---
+++ src/main/java/cn/dbdj1201/interview/thread/CyclicBarrierTest.java
@@ -0,0 +1,33 @@
+package cn.dbdj1201.interview.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @Author: yz1201
+ * @Date: 2022/1/7 8:58
+ */
+public class CyclicBarrierTest {
+
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(3, () -> System.out.println("enough, go go"));
+        System.out.println("game start");
+        int len = 6;
+        for (int i = 0; i < len; i++) {
+            new Thread(() -> {
+                try {
+                    barrier.await();
+
+
+                    System.out.println("help");
+
+
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+        System.out.println("game over");
+    }
+
+}
