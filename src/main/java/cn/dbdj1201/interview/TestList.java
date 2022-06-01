25545667af2ae09e00f6335aa67e744b855c01a5
yz1201 <15957121194@163.com>
2022-06-01 14:57:59 +0800

    lt double20220601

--- src/main/java/cn/dbdj1201/interview/TestList.java
+++ src/main/java/cn/dbdj1201/interview/TestList.java
@@ -1,9 +1,11 @@
 package cn.dbdj1201.interview;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.*;
 
 /**
  * @Author: yz1201
@@ -18,11 +20,58 @@ public class TestList {
 //        paramList.add("asda3");
 //        paramList.add(null);
 //        System.out.println(paramList);
-
-        String key = "params";
-        List<Map<String,String>> paramList  = new ArrayList<>();
-        Map<String,String> m1 = new HashMap<>();
+//
+//        String key = "params";
+//        List<Map<String,String>> paramList  = new ArrayList<>();
+//        Map<String,String> m1 = new HashMap<>();
 //        m1.put();
 
+
+        Set<Map<String,Object>> set = new HashSet<>();
+        Map<String,Object> map1 = new HashMap<>();
+        map1.put("test","test");
+        Map<String,Object> map2 = new HashMap<>();
+        map2.put("test","test");
+        set.add(map1);
+        set.add(map2);
+        System.out.println(set);
+        System.out.println(map1 == map2);
+        System.out.println(map1.hashCode());
+        System.out.println(map2.hashCode());
+        System.out.println(map1);
+        System.out.println(map2);
+        System.out.println(new Object().hashCode());
+
+        System.out.println("===============");
+
+        Person jiezi = new Person("jiezi", 28);
+        Person jiezi1 = new Person("jiezi", 28);
+        Person asd = new Person("asd", 29);
+        System.out.println(jiezi);
+        System.out.println(jiezi1);
+        System.out.println(asd);
+        System.out.println(jiezi.hashCode());
+        System.out.println(jiezi1.hashCode());
+        System.out.println(asd.hashCode());
+
+        Set<Person> personSet = new HashSet<>();
+        personSet.add(jiezi);
+        personSet.add(jiezi1);
+        personSet.add(asd);
+        personSet.add(new Person("jiezi",28));
+        System.out.println(personSet);
+
+
+
     }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Person{
+    private String name;
+    private Integer age;
+
+
 }
---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/ArrayPermute.java
@@ -0,0 +1,39 @@
+package cn.dbdj1201.interview.leetcode.work;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: yz1201
+ * @Date: 2022/6/1 9:19
+ */
+@Slf4j
+public class ArrayPermute {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        //{1,2,3}
+        int length = nums.length;
+        int newLen = length * (length - 1);
+        List<List<Integer>> numsArray = new ArrayList<>(newLen);
+        List<Integer> numArray = new ArrayList<>();
+        for (int i = 0; i < newLen; i++) {
+            //循环打印|整理结果集
+            for (int i1 = 0; i1 < length; i1++) {
+                System.out.print(nums[i1]);
+                System.out.print(" ,");
+                numArray.add(nums[i1]);
+
+            }
+            //交换位置
+
+        }
+
+        return null;
+    }
+}
