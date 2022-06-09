00a465dc516236d337c2d6415760ed5f452b7992
yz1201 <15957121194@163.com>
2022-06-09 14:25:18 +0800

    review bt pre inorder post order, dfs bfs

---
+++ src/main/java/cn/dbdj1201/interview/test/TestMain20220609.java
@@ -0,0 +1,15 @@
+package cn.dbdj1201.interview.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2022/6/9 9:34
+ */
+@Slf4j
+public class TestMain20220609 {
+    public static void main(String[] args) {
+        int[] nums = {};
+        System.out.println(nums[0]);
+    }
+}
