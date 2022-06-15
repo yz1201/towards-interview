101817b19dab1c7ab28a7279d0660ed10f239d22
yz1201 <15957121194@163.com>
2022-06-15 17:04:04 +0800

    copy king -0615

--- src/main/java/cn/dbdj1201/interview/leetcode/work/LetterCombinations.java
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/LetterCombinations.java
@@ -17,7 +17,7 @@ public class LetterCombinations {
     public static void main(String[] args) {
         String s = "23";
         int i = 2;
-        letterCombinations(s);
+        letterCombinationsCopy(s);
     }
 
     public static List<String> letterCombinations(String digits) {
@@ -36,9 +36,43 @@ public class LetterCombinations {
             List<List<Character>> resCharArr = new ArrayList<>();
             for (int i = 0; i < digits.length(); i++) {
                 int key = Integer.parseInt(digits.charAt(i) + "");
-               resCharArr.add(baseMap.get(key));
+                resCharArr.add(baseMap.get(key));
             }
         }
         return null;
+    }
+
+    public static List<String> letterCombinationsCopy(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    public static void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
     }
 }
