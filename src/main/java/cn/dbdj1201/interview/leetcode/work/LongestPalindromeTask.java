06f7d34a6e9ca82d0750b3f9392ec2da60fa3b88
yz1201 <15957121194@163.com>
2022-06-24 17:21:09 +0800

    copy king -0624 sleepy?

--- src/main/java/cn/dbdj1201/interview/leetcode/work/LongestPalindromeTask.java
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/LongestPalindromeTask.java
@@ -113,4 +113,30 @@ public class LongestPalindromeTask {
     }
 
 
+    public static String longestPalindromeCopy2(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenterCopy2(s, i, i);
+            int len2 = expandAroundCenterCopy2(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenterCopy2(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+
+
 }
