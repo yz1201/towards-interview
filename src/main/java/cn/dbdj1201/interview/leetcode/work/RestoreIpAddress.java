85817def6bd741118b28cb34b3c82751a7ea98a4
yz1201 <15957121194@163.com>
2022-07-18 17:31:41 +0800

    0718-copy

--- src/main/java/cn/dbdj1201/interview/leetcode/work/RestoreIpAddress.java
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/RestoreIpAddress.java
@@ -75,9 +75,10 @@ public class RestoreIpAddress {
 
     public static List<String> restoreIpAddressesV1(String s) {
         List<String> ans = new ArrayList<>();
-        String partOne = s.substring(0,1);
+        String partOne = s.substring(0, 1);
         return ans;
     }
+
     private static boolean isLegal(String ip) {
         int length = ip.length();
         if (length == 1) return true;
@@ -93,4 +94,56 @@ public class RestoreIpAddress {
         int res = (int) (part / Math.pow(10, length - 1));
         return res != 0;
     }
+
+
+    static final int SEG_COUNT = 4;
+    static List<String> ans = new ArrayList<>();
+    static int[] segments = new int[SEG_COUNT];
+
+    public static List<String> restoreIpAddressesCopy(String s) {
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    public static void dfs(String s, int segId, int segStart) {
+        // 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuilder ipAddr = new StringBuilder();
+                for (int i = 0; i < SEG_COUNT; ++i) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+
+        // 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+        if (segStart == s.length()) {
+            return;
+        }
+
+        // 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+
+        // 一般情况，枚举每一种可能性并递归
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xFF) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            } else {
+                break;
+            }
+        }
+    }
+
 }
