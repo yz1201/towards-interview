db508e2bf85ed620092a87301c3ea7dfd7edc777
yz1201 <15957121194@163.com>
2022-08-17 21:54:11 +0800

    0817 lt

---
+++ src/main/java/cn/dbdj1201/interview/test/T1832.java
@@ -0,0 +1,14 @@
+package cn.dbdj1201.interview.test;
+
+import cn.dbdj1201.interview.leetcode.work.simple.Q1832;
+
+/**
+ * @Author: yz1201
+ * @Date: 2022/8/17 21:51
+ */
+public class T1832 {
+    public static void main(String[] args) {
+        Q1832 q1832 = new Q1832();
+        q1832.checkIfPangramV1("asd");
+    }
+}
