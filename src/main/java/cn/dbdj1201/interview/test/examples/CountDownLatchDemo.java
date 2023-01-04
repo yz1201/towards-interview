0b472b2bfb6b5a2efeb45ae1f754559faddca113
yz1201 <15957121194@163.com>
2023-01-04 17:02:16 +0800

    alive 20230104 update

---
+++ src/main/java/cn/dbdj1201/interview/test/examples/CountDownLatchDemo.java
@@ -0,0 +1,15 @@
+package cn.dbdj1201.interview.test.examples;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/1/4 15:35
+ */
+@Slf4j
+public class CountDownLatchDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/cn/dbdj1201/interview/test/examples/SemephoreDemo.java
@@ -0,0 +1,43 @@
+package cn.dbdj1201.interview.test.examples;
+
+import cn.hutool.log.Log;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/1/4 14:55
+ */
+@Slf4j
+public class SemephoreDemo {
+
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(5);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        int runs = 10;
+        for (int i = 0; i < runs; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+//                    System.out.println(Thread.currentThread().getName() + "begin");
+                    log.info(Thread.currentThread().getName() + "begin");
+
+                    TimeUnit.MILLISECONDS.sleep(5000);
+
+                    semaphore.release();
+//                    System.out.println(Thread.currentThread().getName() + "end");
+                    log.info(Thread.currentThread().getName() + "end");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        executorService.shutdown();
+        if (executorService.isTerminated())
+            System.out.println("gg");
+    }
+}
