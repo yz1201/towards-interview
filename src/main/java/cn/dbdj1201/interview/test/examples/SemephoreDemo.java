5cff9f77df9d4bfe0b7ec90d9848c902060a9d17
yz1201 <15957121194@163.com>
2023-01-09 09:46:03 +0800

    alive 20230109 update

--- src/main/java/cn/dbdj1201/interview/test/examples/SemephoreDemo.java
+++ src/main/java/cn/dbdj1201/interview/test/examples/SemephoreDemo.java
@@ -1,6 +1,7 @@
 package cn.dbdj1201.interview.test.examples;
 
 import cn.hutool.log.Log;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.*;
@@ -39,5 +40,10 @@ public class SemephoreDemo {
         executorService.shutdown();
         if (executorService.isTerminated())
             System.out.println("gg");
+    }
+
+    @SneakyThrows
+    private void test(){
+
     }
 }
