1df590646644345a0477e3b8d882dde820fa7d60
yz1201 <15957121194@163.com>
2023-03-20 16:32:00 +0800

    alive 20230320 lt 2389 1012

--- src/main/java/cn/dbdj1201/interview/test/simple/T2389.java
+++ src/main/java/cn/dbdj1201/interview/test/simple/T2389.java
@@ -2,6 +2,8 @@ package cn.dbdj1201.interview.test.simple;
 
 import cn.dbdj1201.interview.leetcode.work.simple.Q2389;
 
+import java.util.Arrays;
+
 /**
  * @Author: yz1201
  * @Date: 2023/3/17 16:19
@@ -9,8 +11,11 @@ import cn.dbdj1201.interview.leetcode.work.simple.Q2389;
 public class T2389 {
 
     public static void main(String[] args) {
+        int[] nums = {4,5,2,1};
+        int[] nums1 = {21};
         Q2389 q2389 = new Q2389();
 //        q2389.binarySearch();
+        System.out.println(Arrays.toString(q2389.answerQueriesCopy(nums, nums1)));
     }
 
 }
