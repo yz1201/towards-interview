29197747c4a954c67668d833dd1ed69473f2a6af
yz1201 <15957121194@163.com>
2023-04-03 10:56:41 +0800

    alive 20230403 init

---
+++ src/main/java/cn/dbdj1201/interview/test/Test20230329.java
@@ -0,0 +1,37 @@
+package cn.dbdj1201.interview.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/3/29 10:17
+ */
+@Slf4j
+public class Test20230329 {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(5);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        int len = 11;
+        for (int i = 0; i < len; i++) {
+            executorService.submit(() -> {
+                if (semaphore.tryAcquire())
+                    log.info("  hello");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                    log.info("free");
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
