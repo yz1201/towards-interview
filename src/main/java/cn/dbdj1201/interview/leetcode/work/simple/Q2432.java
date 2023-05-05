3152bd32df499d2bd4885aacc56e791852cbc932
yz1201 <15957121194@163.com>
2023-05-05 22:37:44 +0800

    alive 20230505 970

---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/simple/Q2432.java
@@ -0,0 +1,16 @@
+package cn.dbdj1201.interview.leetcode.work.simple;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/5/5 8:58
+ */
+@Slf4j
+public class Q2432 {
+
+    public int hardestWorker(int n, int[][] logs) {
+
+        return -1;
+    }
+}
---
+++ src/main/java/cn/dbdj1201/interview/test/medium/T970.java
@@ -0,0 +1,16 @@
+package cn.dbdj1201.interview.test.medium;
+
+import cn.dbdj1201.interview.leetcode.work.middle.M970;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/5/5 16:12
+ */
+@Slf4j
+public class T970 {
+    public static void main(String[] args) {
+        M970 m970 = new M970();
+        System.out.println(m970.powerfulIntegers(2, 3, 10));
+    }
+}
