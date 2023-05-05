0ebacf235dbe6e8c623ab6dce28b0444a86aaec7
yz1201 <15957121194@163.com>
2023-05-05 08:55:02 +0800

    alive 20230505 init

---
+++ src/main/java/cn/dbdj1201/interview/test/difficult/T1172.java
@@ -0,0 +1,22 @@
+package cn.dbdj1201.interview.test.difficult;
+
+import cn.dbdj1201.interview.leetcode.work.diffcult.DinnerPlates;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/4/28 10:16
+ */
+@Slf4j
+public class T1172 {
+
+    public static void main(String[] args) {
+        DinnerPlates dinnerPlates = new DinnerPlates(2);
+        dinnerPlates.push(2);
+        dinnerPlates.push(2);
+        dinnerPlates.push(2);
+        dinnerPlates.push(2);
+        dinnerPlates.push(2);
+        dinnerPlates.push(2);
+    }
+}
