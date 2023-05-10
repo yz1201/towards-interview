5ebc23b4cabc21f0a68bd50f416c83cac597fe61
yz1201 <15957121194@163.com>
2023-05-10 14:55:35 +0800

    alive 20230510 init

---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/middle/M1015.java
@@ -0,0 +1,15 @@
+package cn.dbdj1201.interview.leetcode.work.middle;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/5/10 14:55
+ */
+@Slf4j
+public class M1015 {
+
+    public int smallestRepunitDivByK(int k) {
+        return -1;
+    }
+}
