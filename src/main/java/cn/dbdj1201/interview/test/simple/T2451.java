da041c70c20f84135cfba3856dc22b42c255b256
yz1201 <15957121194@163.com>
2023-06-06 09:56:43 +0800

    alive 20230606 M2352 complete

--- src/main/java/cn/dbdj1201/interview/test/simple/T2451.java
+++ src/main/java/cn/dbdj1201/interview/test/simple/T2451.java
@@ -3,6 +3,8 @@ package cn.dbdj1201.interview.test.simple;
 import cn.dbdj1201.interview.leetcode.work.simple.S2451;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -19,13 +21,19 @@ public class T2451 {
     public static void main(String[] args) {
         S2451 s2451 = new S2451();
         String[] words = {"aaaaa", "zzzzz", "bbbbb","ccccc","ddddd","abced"};
-        System.out.println(s2451.oddStringCopy(words));
-        Set<int[]> set = new HashSet<>();
-        System.out.println(set.add(new int[]{1, 2}));
-        System.out.println(set.add(new int[]{1, 2}));
-        System.out.println(set.add(new int[]{1, 2}));
-        System.out.println(set.add(new int[]{1, 2}));
+//        System.out.println(s2451.oddStringCopy(words));
+//        Set<int[]> set = new HashSet<>();
+//        System.out.println(set.add(new int[]{1, 2}));
+//        System.out.println(set.add(new int[]{1, 2}));
+//        System.out.println(set.add(new int[]{1, 2}));
+//        System.out.println(set.add(new int[]{1, 2}));
         List<String> strings = Arrays.stream(words).collect(Collectors.toList());
+        String s = "1111";
+        System.out.println(s.charAt(3) == '1');
+
+        String dateStr = "20991231";
+
+        System.out.println(LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyyMMdd")));
 
     }
 }
