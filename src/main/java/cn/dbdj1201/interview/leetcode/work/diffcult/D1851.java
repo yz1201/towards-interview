28bc832809a11e1cc7985ccda8563712ba6f8e9e
yz1201 <dbdj1201@gmail.com>
2023-07-20 15:02:07 +0800

    alive 20230720 2540 test, others just start.

---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/diffcult/D1851.java
@@ -0,0 +1,16 @@
+package cn.dbdj1201.interview.leetcode.work.diffcult;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/7/19 15:19
+ */
+@Slf4j
+public class D1851 {
+
+    public int[] minInterval(int[][] intervals, int[] queries) {
+
+        return null;
+    }
+}
