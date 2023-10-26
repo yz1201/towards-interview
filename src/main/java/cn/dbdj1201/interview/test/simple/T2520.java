b4b8f5d4bd65dc33d9e25641db85fcedbf9b7737
yz1201 <dbdj1201@gmail.com>
2023-10-26 14:17:17 +0800

    alive 20231026 2520 easy

---
+++ src/main/java/cn/dbdj1201/interview/test/simple/T2520.java
@@ -0,0 +1,21 @@
+package cn.dbdj1201.interview.test.simple;
+
+import cn.dbdj1201.interview.leetcode.work.simple.S2520;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/10/26 11:13
+ */
+@Slf4j
+public class T2520 {
+
+    public static void main(String[] args) {
+        S2520 s2520 = new S2520();
+        System.out.println(s2520.countDigits(7));
+
+        char c = '1';
+        int b = Integer.parseInt(String.valueOf(c));
+        System.out.println(b);
+    }
+}
