7c228c5522797ac54f418b15e311f12c61676925
yz1201 <dbdj1201@gmail.com>
2023-12-22 14:23:15 +0800

    alive juc test reentrant lock

--- src/main/java/cn/dbdj1201/interview/thread/AlternatePrintTest2.java
+++ src/main/java/cn/dbdj1201/interview/thread/AlternatePrintTest2.java
@@ -17,9 +17,9 @@ public class AlternatePrintTest2 {
         Condition a_condition = awaitSignal.newCondition();
         Condition b_condition = awaitSignal.newCondition();
         Condition c_condition = awaitSignal.newCondition();
-        new Thread(() -> awaitSignal.print("a", a_condition, b_condition), "Thread-a").start();
-        new Thread(() -> awaitSignal.print("b", b_condition, c_condition), "Thread-b").start();
-        new Thread(() -> awaitSignal.print("c", c_condition, a_condition), "Thread-c").start();
+        new Thread(() -> awaitSignal.printV1("a", a_condition, b_condition), "Thread-a").start();
+        new Thread(() -> awaitSignal.printV1("b", b_condition, c_condition), "Thread-b").start();
+        new Thread(() -> awaitSignal.printV1("c", c_condition, a_condition), "Thread-c").start();
         System.out.println("开始...");
         Thread.sleep(10);
         awaitSignal.lock();
@@ -69,4 +69,34 @@ class AwaitSignal extends ReentrantLock {
             unlock();
         }
     }
+
+
+    /**
+     * print
+     *
+     * @param str     打印的内容
+     * @param current 进入那间休息室
+     * @param next    下一间休息室
+     */
+    public void printV1(String str, Condition current, Condition next) {
+        for (int i = 0; i < loopNumber; i++) {
+            lock();
+            try {
+                try {
+                    current.await();
+                    System.out.print(str);
+//                    log.info("str - " + str);
+                    counter++;
+                    // 唤醒下一间休息室
+                    next.signal();
+                    log.info("counter : {}", this.getCounter());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } finally {
+                unlock();
+            }
+        }
+    }
+
 }
