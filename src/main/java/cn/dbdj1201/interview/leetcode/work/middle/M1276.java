7ea0277bd425c5ec92b8c0631b1d4d853f817d89
yz1201 <dbdj1201@gmail.com>
2023-12-25 17:57:25 +0800

    alive m1276 cp math

---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/middle/M1276.java
@@ -0,0 +1,25 @@
+package cn.dbdj1201.interview.leetcode.work.middle;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/12/25 15:52
+ */
+@Slf4j
+public class M1276 {
+    public List<Integer> numOfBurgersCopy(int tomatoSlices, int cheeseSlices) {
+        if (tomatoSlices % 2 != 0 || tomatoSlices < cheeseSlices * 2 || cheeseSlices * 4 < tomatoSlices) {
+            return new ArrayList<>();
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        ans.add(tomatoSlices / 2 - cheeseSlices);
+        ans.add(cheeseSlices * 2 - tomatoSlices / 2);
+        return ans;
+    }
+
+
+}
---
+++ src/main/java/cn/dbdj1201/interview/test/medium/T1276.java
@@ -0,0 +1,17 @@
+package cn.dbdj1201.interview.test.medium;
+
+import cn.dbdj1201.interview.leetcode.work.middle.M1276;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: yz1201
+ * @Date: 2023/12/25 16:28
+ */
+@Slf4j
+public class T1276 {
+
+    public static void main(String[] args) {
+        M1276 m1276 = new M1276();
+        m1276.numOfBurgersCopy(18, 4);
+    }
+}
