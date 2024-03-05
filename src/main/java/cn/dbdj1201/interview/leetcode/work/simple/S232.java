37c0808b86c96fd9af9095d4637dca79ba5c569e
yz1201 <dbdj1201@gmail.com>
2024-03-05 17:28:57 +0800

    alive s232 cant understand?

---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/simple/S232.java
@@ -0,0 +1,50 @@
+package cn.dbdj1201.interview.leetcode.work.simple;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+/**
+ * @Author: yz1201
+ * @Date: 2024/3/5 11:02
+ */
+@Slf4j
+public class S232 {
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+
+    public S232() {
+        inStack = new ArrayDeque<Integer>();
+        outStack = new ArrayDeque<Integer>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void in2out() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
+    }
+}
