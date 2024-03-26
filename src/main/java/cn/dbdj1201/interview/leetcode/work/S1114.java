596660e51d24176a56884bd08af0b015556da222
yz1201 <dbdj1201@gmail.com>
2024-03-26 14:46:42 +0800

    alive s1114 juc

---
+++ src/main/java/cn/dbdj1201/interview/leetcode/work/S1114.java
@@ -0,0 +1,39 @@
+package cn.dbdj1201.interview.leetcode.work;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author: yz1201
+ * @Date: 2024/3/26 9:50
+ */
+@Slf4j
+public class S1114 {
+    private final CountDownLatch firstDone;
+    private final CountDownLatch secondDone;
+
+    public S1114() {
+        firstDone = new CountDownLatch(1);
+        secondDone = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        firstDone.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        firstDone.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        secondDone.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        secondDone.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
