1442ce62703eacdfb55ecb7af898306124784f13
yz1201 <dbdj1201@gmail.com>
2024-05-15 10:06:54 +0800

    test commit -2

--- src/main/java/cn/dbdj1201/interview/test/TestMain2024051501.java
+++ src/main/java/cn/dbdj1201/interview/test/TestMain2024051501.java
@@ -9,6 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TestMain2024051501 {
     public static void main(String[] args) {
-        System.out.println("test commit");
+        System.out.println("test commit 111");
     }
 }
